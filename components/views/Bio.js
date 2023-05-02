a5ce2eb82d8971de79670d22827faff099d39dce
lrandolph23 <lacy.randolph23@gmail.com>
2023-05-01 21:38:14 -0500

    Pizza

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,5 +1,5 @@
 import html from "html-literal";
-import dogPic from "../../assets/img/photo-1510771463146-e89e6e86560e.jpg"
+import dogPic from "../../assets/img/photo-1510771463146-e89e6e86560e.jpg";
 export default () => html`
 <section id="bio">
 <h2>Vivamus ac justo eu nisi</h2>
--- store/Pizza.js
+++ store/Pizza.js
@@ -1,4 +1,5 @@
 export default {
   header: "MaAKe MeE A pIzzZaA!",
   view: "Pizza",
+  pizzas: []
 };
